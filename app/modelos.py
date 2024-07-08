c8158f0c5c849dfe563039038e94f2d8c5030640
HernánRG <hernanrg85@gmail.com>
2024-07-08 22:12:31 +0200

    añadida clase grupo_entrada

--- app/modelos.py
+++ app/modelos.py
@@ -1,22 +1,28 @@
 from enum import Enum, auto
 
 class TipoEntrada(Enum):
-    # BEBE = auto()
-    # NIÑO = auto()
-    # ADULTO = auto()
-    # JUBILADO = auto()
+    BEBE = auto()
+    NIÑO = auto()
+    ADULTO = auto()
+    JUBILADO = auto()
+    NO_VALIDO = auto()
 
-    BEBE = {"EDAD": 3, "PRECIO": 0, "CONTADOR": 0}
-    NIÑO = {"EDAD": 13, "PRECIO": 14, "CONTADOR": 0}
-    ADULTO = {"EDAD": 65, "PRECIO": 23, "CONTADOR": 0}
-    JUBILADO = {"EDAD": float('inf'), "PRECIO": 18, "CONTADOR": 0}
+    # BEBE = {"EDAD": 3, "PRECIO": 0, "CONTADOR": 0}
+    # NIÑO = {"EDAD": 13, "PRECIO": 14, "CONTADOR": 0}
+    # ADULTO = {"EDAD": 65, "PRECIO": 23, "CONTADOR": 0}
+    # JUBILADO = {"EDAD": float('inf'), "PRECIO": 18, "CONTADOR": 0}
+
+
 
 class Entrada():
-    def __init__(self, edad: int) -> None:
+    def __init__(self, edad: int):
         self.edad = edad
-        if edad <= 2:
+        if edad < 0:
+            self.tipo = TipoEntrada.NO_VALIDO
+            self.precio = 0
+        elif edad <= 2:
             self.tipo = TipoEntrada.BEBE
-            self.precio = TipoEntrada.BEBE.value["PRECIO"]
+            self.precio = 0
         elif edad < 13:
             self.tipo = TipoEntrada.NIÑO
             self.precio = 14
@@ -26,3 +32,18 @@ class Entrada():
         else: 
             self.tipo = TipoEntrada.JUBILADO
             self.precio = 18
+
+class Grupo_Entrada():
+    def __init__(self):
+        self.total = 0
+        self.num_entradas = 0
+
+    def add_entrada(self, edad):
+        """En la funcion de la edad, crear una entrada e incrementar 
+        entradas con el precio de la entrada nueva incrementar el total"""
+
+        entrada = Entrada(edad)
+        
+        if entrada.tipo != TipoEntrada.NO_VALIDO:
+            self.num_entradas += 1
+            self.total += entrada.precio
--- tests/test_modelos.py
+++ tests/test_modelos.py
@@ -1,4 +1,4 @@
-from app.modelos import Entrada, TipoEntrada  
+from app.modelos import Entrada, TipoEntrada, Grupo_Entrada  
 
 def test_crear_entrada():
     entrada = Entrada(2)
@@ -17,6 +17,36 @@ def test_crear_entrada():
     assert entrada.tipo == TipoEntrada.JUBILADO
     assert entrada.precio == 18
 
-def test_crear_entradas_negativas():
+def test_crear_entrada_edad_negativa():
     entrada = Entrada(-2)
-    
+    assert entrada.tipo == TipoEntrada.NO_VALIDO
+    assert entrada.precio == 0
+
+def test_crear_grupo_entradas():
+    grupo = Grupo_Entrada()
+    assert grupo.total == 0
+    assert grupo.num_entradas == 0
+
+def test_anadir_entradas_a_grupo():
+    grupo = Grupo_Entrada()
+    grupo.add_entrada(23)
+    assert grupo.num_entradas == 1
+    assert grupo.total == 23 
+
+    grupo.add_entrada(12)
+    assert grupo.num_entradas == 2
+    assert grupo.total == 37
+
+    grupo.add_entrada(70)
+    assert grupo.num_entradas == 3
+    assert grupo.total == 55
+
+    grupo.add_entrada(2)
+    assert grupo.num_entradas == 4
+    assert grupo.total == 55
+
+    grupo.add_entrada(-9)
+    assert grupo.num_entradas == 4
+    assert grupo.total == 55
+
+
